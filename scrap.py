2f33d125107dffd1959f053c3c8923f743254531
Arnab Das <arnabdas.9039@gmail.com>
2024-08-27 13:27:48 +0530

    Added Chat

--- scrap.py
+++ scrap.py
@@ -109,77 +109,30 @@ followings.remove('')
 print("Number of Followings: ",len(followings))
 print("List of Followings", followings)
 
-last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight',msg_list)
     time.sleep(2)
-    new_height = driver.execute_script("return document.body.scrollHeight")
+    new_height = driver.execute_script("return arguments[0].scrollHeight", msg_list)
     if new_height == last_height:
         break
     last_height = new_height
+    # msg_user = driver.find_elements(By.XPATH, '//*[@id="mount_0_0_yt"]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/section/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div/div/div[1]/div/div/div/div/div/div')
+    # msg_height = driver.execute_script("return arguments[0].scrollHeight", msg_user)
 
-# Find posts in the DOM
-posts = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/")]')
+    # while True:
+        # driver.execute_script('arguments[0]')
 
-# Loop through each post
-for post_index, post in enumerate(posts):
-    post_url = post.get_attribute('href')
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(post_url)
-    post_id = post_url.replace('https://www.instagram.com', '')
 
-    time.sleep(2)
+chatting_elements = msg_list.find_elements(By.XPATH, '//div[@role="listitem"]')
+chatters = []
+# chatters = [ chatting_element.text for chatting_element in chatting_elements]
+print(chatting_elements)
+for chatting_element in chatting_elements:
+#     print(chatting_element)
+    name = chatting_element.text
+    chatters.append(name)
 
-    try:
-        media_elements = driver.find_element(By.XPATH, '//div[@role="button"]//img')
-        media_element_time = driver.find_element(By.XPATH, f'//a[@href="{post_id}"]//time').get_attribute('datetime')
+print(chatters)
 
-        print(media_element_time)
-        # Handle multiple images or videos in a carousel post
-        try:
-            presentation = driver.find_element(By.XPATH, '//div[@role="presentation"]')
-            media_index = 0
-            unique_urls = set()
-            while True:
-                time.sleep(2)
-                media_elements = presentation.find_elements(By.XPATH, "//li//img")
-                
-                for media_element in media_elements:
-                    media_url = media_element.get_attribute('src')
-                    unique_urls.add(media_url)
-                try:
-                    next_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Next"]'))
-                    )
-                    next_button.click()
-                    media_index += 1
-                except Exception:
-                    break
-            
-            # Download the images
-            for media_index, media_url in enumerate(unique_urls):
-                response = requests.get(media_url)
-                if response.status_code == 200:
-                    with open(f'post_{post_index + 1}_media_{media_index + 1}.jpg', 'wb') as file:
-                        file.write(response.content)
-                print(f"Post {post_index + 1}, media {media_index + 1} downloaded successfully from {media_url}")
-
-        except Exception:
-            media_url = media_elements.get_attribute('src')
-
-            response = requests.get(media_url)
-            if response.status_code == 200:
-                with open(f'post_{post_index + 1}.jpg', 'wb') as file:
-                    file.write(response.content)
-            print(f"Post {post_index + 1} downloaded successfully from {media_url}")
-
-    except Exception as e:
-        print(f"Error processing post {post_index + 1}: {e}")
-
-    # Close the current tab and switch back to the original tab
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    
 # Close the browser
 driver.quit()

1e7df6f2e361cccee6d51cce704185701f7718ab
Tridibesh Sarkar <tridibeshsarkar07@gmail.com>
2024-09-08 02:22:18 +0530

    hii mina saaaaan ,we are espadas

--- scrap.py
+++ scrap.py
@@ -22,8 +22,8 @@ password = driver.find_element(By.NAME, 'password')
 
 # your_username = 'iamsatwik_'
 # your_password =  'humming1bird'
-your_username = 'deekshaaa_ch'
-your_password =  'gaynigga@69'
+your_username = 'tridibesh9'
+your_password =  '098jhgcxz'
 
 username.send_keys(your_username)
 password.send_keys(your_password)
@@ -191,7 +191,7 @@ notify_close.click()
 
 msg_list = driver.find_element(By.XPATH, '//div[@role="list"]/div/div/div')
 last_height = driver.execute_script("return arguments[0].scrollHeight", msg_list)
-chatters = set()
+chatters = []
 attempt = 0
 scroll_pause_time = 5
 
@@ -199,8 +199,22 @@ while attempt < 5:
     chatting_elements = msg_list.find_elements(By.XPATH, '//div[@role="listitem"]')
     # chatters.append(chatting_element.text for chatting_element in chatting_elements)
     for chatting_element in chatting_elements:
-        name = chatting_element.text
-        chatters.add(name)
+        chatter_info = {}
+        try:
+            # Extracting username or relevant field
+            chatter_element = chatting_element.find_elements(By.XPATH, './/span[@dir="auto"]')
+            username = chatter_element[0].text
+            chatter_info['username'] = username
+
+            # Extracting other info, such as message or time (example)
+            message = chatter_element[1].text
+            chatter_info['message'] = message
+
+        except Exception as e:
+            print(f"Error extracting fields: {e}")
+
+        # Add the dictionary to the list of chatters
+        chatters.append(chatter_info)
 
     driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', msg_list)
     time.sleep(scroll_pause_time)
@@ -215,12 +229,16 @@ while attempt < 5:
     # msg_height = driver.execute_script("return arguments[0].scrollHeight", msg_user)
 
     # while True:
-        # driver.execute_script('arguments[0]')
+    # driver.execute_script('arguments[0]')
 
     # print(chatting_element.text)
+unique_chatters = set(tuple(chatter.items()) for chatter in chatters)
+unique_chatters = [dict(chatter) for chatter in unique_chatters]
 
-print(chatters)
-print(len(chatters))
+print(unique_chatters)
+print(len(unique_chatters))
+
+
 
 # Close the browser
 driver.quit()
